c09812c1d01bc01850f5a5a30c95f0e26b82ce4b
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-06-29 23:51:40 +0300

    Task4 file added

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,4 +1,13 @@
 ﻿int number1;
 
 Console.WriteLine("Imput number: ");
+number1 = Convert.ToInt32(Console.ReadLine());
 
+if (number1%2 == 0)
+{
+    Console.Write(number1 + " число четное!");
+}
+else
+{
+    Console.Write(number1 + " число нечетное");
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа принимает на вход число N,  а на выходе показывает все четные числа от 1 до N
+
+int num;
+
+Console.WriteLine("Input your number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<=num; i++) {
+    if(i%2 == 0) {
+        Console.Write("{0} ", i);
+    }
+}
